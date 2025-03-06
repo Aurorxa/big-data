f248c28ec47ae222bc1ead39421309ca7f548db3
许大仙 <1900919313@qq.com>
2025-03-06 09:48:23 +0800

    初始化

---
+++ docs/.vitepress/theme/components/TypeIt.vue
@@ -0,0 +1,14 @@
+<template></template>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import TypeIt from "typeit";
+
+onMounted(() => {
+  new TypeIt(".tagline", {
+    strings: [
+      "处理和分析海量、多样、高速增长的数据集，以提取有价值的信息，常用于决策支持和预测分析。",
+    ],
+    speed: 200,
+  }).go();
+});
+</script>
